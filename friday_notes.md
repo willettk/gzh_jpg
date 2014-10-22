bd5a2eb3ae3190783624e721d323a0973bb36c1d
Kyle Willett <willettk@gmail.com>
2014-10-22 12:27:35 -0500

    Desaturated color addressed

--- friday_notes.md
+++ friday_notes.md
@@ -1,7 +1,18 @@
 Notes from discussion between Brooke and Kyle on Fri 27 Jun 2014
 ===
 
-* repixelate images to 424x424
-* examine the stretch and appropriate values for each band
-* Steven's noise decoloring with a raised floor; either import to IDL or add as Python
-** Nonlinearity level of 3.0 +- 0.5 seems appropriate to bring out detail and raise noise floor a bit. 
+* [done] repixelate (larger) images to 424x424 after scaling to fixed fraction of galaxy radius
+* [done] examine the stretch and appropriate values for each band
+* [done] Noise decoloring (Bamford method) with a raised floor
+** [done] Nonlinearity level of 3.0 +- 0.5 seems appropriate to bring out detail and raise noise floor a bit. 
+
+* [need catalog] Make the FULL GOODS-S catalog, which should be about 6,000 galaxies (in excess of the 500 random sample)
+* [need catalog] Cut on magnitudes less than 24.5 in z-band
+* [need GZH results] Remove objects flagged as stars/artifacts; suggest CLASS_STELLAR_MAX >= 0.90 to start? Would leave roughly 20% of images
+* [need catalog] Add GOODS-N
+
+Fixed and updated the PIL software (required upgrade to Pillow and some changes in how Image is imported in code)
+
+citation for GOODS data is Giavalisco+04
+
+make multiple versions (cols 1 and 3) so that we can point to the "original" versions if there are questions on artifacts
--- python/make_color_bviz_jpeg.py
+++ python/make_color_bviz_jpeg.py
@@ -1,10 +1,11 @@
 __credits__ = '''Written by Kyle Willett (willettk@gmail.com); adapted from code by Taro Sato, Steven Bamford, and Brooke Simmons'''
 
-import Image as I
+from PIL import Image as I      # Use Pillow instead of the default PIL or Image library
 import numpy as np
 import nw
 from scipy.misc import imresize
 from matplotlib import pyplot as plt
+from copy import deepcopy
 
 try:
     from astropy.io import fits
@@ -16,13 +17,23 @@ basepath = '..'
 jpg_path  = '%s/jpg_thumb/from_mosaics' % basepath
 tiff_path = '%s/tiff_thumb/from_mosaics' % basepath
 fits_path = '%s/fits_thumb' % basepath
-mosaic_path = '/Volumes/BDS_backup/Astro/GOODS/v1.0/images'
+#mosaic_path = '/Volumes/BDS_backup/Astro/GOODS/v1.0/images'
+mosaic_path = '/Users/willettk/Astronomy/meetings/uk2014/gzh_jpg/GOODS_mosaics/goods-s'
 
-def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=False,show_img=True,load_from_mosaic=False,mosaics=None):
+random_fname = 'randomselect_uniformwithmag_basicinfo.fits'
+goods_s_fname = 'goods_s_all.fits'
+goods_n_fname = 'goods_n_all.fits'
+
+finput_name = random_fname
+ns = 's'                    # Specify 'n' for GOODS-N, 's' for GOODS-S
+
+def make_jpeg(gal,color_scheme='bviz',desaturate=False,show_img=True,load_from_mosaic=False,mosaics=None):
+
+    ascales,anonlinearity,npix_final,apedestal = get_image_defaults(color_scheme=color_scheme)
 
     input_oid = gal['UID_MOSAIC']
     id_str = 'g%s' % input_oid
-    print '----------------- %s --------------------' % id_str
+    #print '----------------- %s --------------------' % id_str
 
 
     # Get the amount by which we need to resize the image
@@ -32,7 +43,7 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
     obj_size_pix = a_pix * kron_r
 
     # Scale in HST pixel size; go out to 2.5 times the Kron radius (standard aperture for SExtractor)
-    img_size_hstpix = 2.5 * obj_size_pix
+    img_size_hstpix = 3.5 * obj_size_pix
 
     # Sanity checks -- don't zoom in or out too far
     img_size_hstpix = max(img_size_hstpix, 120.)
@@ -41,20 +52,28 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
     resize_factor = npix_final / img_size_hstpix
 
     # Print re-sizing parameters to screen
-    print 'Semi-major axis [pix]: %f\n Kron radius [pix]: %f\n Image size [pix]: %f\n Resize factor: %f' % (a_pix, kron_r*a_pix, img_size_hstpix, resize_factor)
+    print '%s -- Semi-major axis [pix]: %f\n Kron radius [pix]: %f\n Image size [pix]: %f\n Resize factor: %f' % (id_str,a_pix, kron_r*a_pix, img_size_hstpix, resize_factor)
+    '''
+    if resize_factor < 1:
+        # Resizing means that it has to be extracted from the mosaic image and then actually DOWNGRADED from the existing resolution
+        # to fit a 424x424 image. Done automatically if extracted from mosaic.
+        print '%10s - Resize factor: %.3f' % (id_str,resize_factor)
+    else:
+        print '%10s - no resize necessary:  %.3f' % (id_str,resize_factor)
+    '''
 
     # Load FITS data from original GOODS mosaics
 
     if load_from_mosaic:
         if mosaics is None:
-            osect_z = gal['OSECT_Z']
-            with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'b',osect_z)) as f:
+            osect = gal['OSECT_Z']
+            with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'b',osect)) as f:      # Note: will work w/gzipped data, but MUCH slower.
                 img_b = np.transpose(f[0].data)
-            with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'v',osect_z)) as f:
+            with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'v',osect)) as f:
                 img_v = np.transpose(f[0].data)
-            with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'i',osect_z)) as f:
+            with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'i',osect)) as f:
                 img_i = np.transpose(f[0].data)
-            with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'z',osect_z)) as f:
+            with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'z',osect)) as f:
                 img_z = np.transpose(f[0].data)
         else:
             img_b,img_v,img_i,img_z = mosaics
@@ -90,9 +109,14 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
 
     # If one out of four images doesn't exist, just erase the color (NOT YET IMPLEMENTED)
     
-    rimage = img_z
-    gimage = img_i
-    bimage = np.array([img_b,img_v]).mean(axis=0)
+    if color_scheme == 'bviz':
+        rimage = img_z
+        gimage = img_i
+        bimage = np.array([img_b,img_v]).mean(axis=0)
+    elif color_scheme == 'vz':
+        rimage = img_z
+        gimage = np.array([img_z,img_v]).mean(axis=0)
+        bimage = img_v
 
     nx,ny = rimage.shape
 
@@ -109,16 +133,29 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
     if desaturate:
         # optionally desaturate pixels that are dominated by a single
         # colour to avoid colourful speckled sky
+
+        orig = deepcopy(RGBim)
+        # Take the mean flux value between the three color bands
         a = RGBim.mean(axis=0)
+        # mask pixels with no flux in any of the bands
         np.putmask(a, a == 0.0, 1.0)
+        # create cube with each plane containing mean flux value
         acube = np.resize(a,(3,nx,ny))
+        # scale each color to the mean, then divide by non-linearity factor
         bcube = (RGBim / acube) / anonlinearity
+        # create a mask based on the weighted non-linear color values
         mask = np.array(bcube)
-        w = np.max(mask, 0)
+        # find the maximum weighted value per color
+        w = np.max(mask,axis=0)
+        # if the max value is greater than 1, replace with 1
         np.putmask(w, w > 1.0, 1.0)
+        # invert mapping from 0 to 1.
         w = 1 - w
+        # taper the weights with a sin function
         w = np.sin(w*np.pi/2.0)
-        RGBim = RGBim * w + a*(1-w)
+        # multiply the original image by the normalized weights, add back the weighted mean flux, and [optionally] recolor strongest pixels
+        #RGBim = RGBim * w + a*(1-w)
+        RGBim = RGBim * w + a*(1-w) + a*(1-w)**2 * orig
 
     # Convert data to scaled bytes
     RGBim = (255.*RGBim).astype(int)
@@ -127,7 +164,7 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
 
     # Add optional grey pedestal to the byte-scaled data
 
-    RGBim += pedestal
+    RGBim += apedestal
     RGBim = np.where(RGBim>255,255,RGBim)
 
     R = RGBim[0,:,:]
@@ -153,54 +190,76 @@ def make_jpeg(gal,ascales,anonlinearity,npix_final=424,pedestal=0,desaturate=Fal
         img.show()
 
     # Save hardcopy as both JPG and TIFF
-    out_jpg = '%s/%s_bviz_thumb.jpg' % (jpg_path,id_str)
-    out_tiff = '%s/%s_bviz_thumb.tiff' % (tiff_path,id_str)
+    out_jpg  = '%s/%s/goods_%s_%s_%s_thumb.jpg'  % ( jpg_path,color_scheme,ns,id_str,color_scheme)
+    out_tiff = '%s/%s/goods_%s_%s_%s_thumb.tiff' % (tiff_path,color_scheme,ns,id_str,color_scheme)
     tiff_desc = {'RA':gal['ALPHA_J2000_B'],'DEC':gal['DELTA_J2000_B']}
 
-    img.save(out_jpg,quality=100)
-    img.save(out_tiff,quality=100,tiffinfo=tiff_desc)
+    img.save(out_jpg,format='JPEG',quality=100)
+    #img.save(out_tiff,quality=100,tiffinfo=tiff_desc)        
 
     return None
 
 def run_all_images(load_from_mosaic=True):
 
-    ascales,anonlinearity,npix_final,apedestal = get_image_defaults()
-   
-    finput_name = 'randomselect_uniformwithmag_basicinfo.fits'
-
     with fits.open('%s/%s' % (basepath,finput_name)) as f:
         finput_data = f[1].data
     
+    '''
+    Select only galaxies brighter than 24.5 mag in z-band. Fainter than this,
+    there are almost galaxies with any visible features. Note this is 1 mag fainter than
+    the limit originally used for GEMS and GOODS-N in GZ: Hubble
+    '''
+    mag_cut = (finput_data['MAG_BEST_Z'] <= 24.5) & (finput_data['MAGERR_BEST_Z'] < 1.)
+
+    # Remove objects identified by GOODS pipeline as highly likely to be a star
+    star_cut = (np.max([finput_data['CLASS_STAR_B'],finput_data['CLASS_STAR_V'],finput_data['CLASS_STAR_I'],finput_data['CLASS_STAR_Z']],axis=0) < 0.90) & \
+        (np.mean([finput_data['CLASS_STAR_B'],finput_data['CLASS_STAR_V'],finput_data['CLASS_STAR_I'],finput_data['CLASS_STAR_Z']],axis=0) <= 0.75)
+
+    data = finput_data[mag_cut & star_cut]
     n_imgs = 0
 
-    # Load mosaic files by OSECT_Z to save on memory reads
+    # Load mosaic files only as needed to save on memory reads
     if load_from_mosaic:
-        osects = np.unique(finput_data['OSECT_Z'])
+        osects = np.unique(data['OSECT_Z'])
+        # Loop over mosaic images
         for o in osects:
-            osect_galaxies = finput_data[finput_data['OSECT_Z'] == o]
+            osect_galaxies = data[data['OSECT_Z'] == o]
+            # Loop over galaxies in mosaic
             for gal in osect_galaxies:
-                with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'b',o)) as f:
+                with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'b',o)) as f:
                     img_b = np.transpose(f[0].data)
-                with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'v',o)) as f:
+                with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'v',o)) as f:
                     img_v = np.transpose(f[0].data)
-                with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'i',o)) as f:
+                with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'i',o)) as f:
                     img_i = np.transpose(f[0].data)
-                with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'z',o)) as f:
+                with fits.open('%s/%s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,ns,'z',o)) as f:
                     img_z = np.transpose(f[0].data)
-                make_jpeg(gal,ascales,anonlinearity,npix_final,apedestal,desaturate=False,show_img = False,\
+
+                # Make the JPEG image
+                make_jpeg(gal,\
+                    color_scheme='bviz',
+                    desaturate=True,show_img = False,\
                     load_from_mosaic = True, mosaics = (img_b,img_v,img_i,img_z))
+                '''
+                make_jpeg(gal,\
+                    color_scheme='vz',
+                    desaturate=True,show_img = False,\
+                    load_from_mosaic = True, mosaics = (img_b,img_v,img_i,img_z))
+                '''
                 n_imgs += 1
 
-    # Loading from Brooke's FITS cutouts
+    # Load from FITS cutouts instead of the mosaic (faster, but zooms in too much for the largest angular size galaxies)
     else:
         for gal in finput_data:
-            make_jpeg(gal,ascales,anonlinearity,npix_final,apedestal,desaturate=False,show_img = False,load_from_mosaic = False)
+            make_jpeg(gal,desaturate=False,show_img = False,load_from_mosaic = False)
             n_imgs += 1
     
-    print 'Created %i images in both JPG and TIFF formats.' % n_imgs
+    print 'Created %i images in JPG format.' % n_imgs
     print 'Done.'
 
-def get_image_defaults():
+    return None
+
+def get_image_defaults(color_scheme='bviz'):
 
     # set parameters - pretty much trial and error here
     # for H (f160w), (H+J)/2, J (f125w)
@@ -213,7 +272,8 @@ def get_image_defaults():
     
     
     # for (B+V)/2, I, z
-    ascales= 1.75*np.array([45, 22, 28])
+    if color_scheme == 'bviz':
+        ascales= 1.75*np.array([45, 22, 28])
     
     
     # for V, (V+I)/2, I
@@ -222,9 +282,14 @@ def get_image_defaults():
     #scales= 2*[15,10,8]
     #scales= 1.5*[26,22,21]
     #scales= 2.5*[16.,11.,11.]
+    if color_scheme == 'vz':
+        ascales= 2.5*np.array([16.,11.,11.])
+        #ascales= 2.0*np.array([15,10,8])
+        #ascales= 1.5*np.array([26,22,21])
+
     # according to Roger Griffith's program (also adapted from Hogg's)
     #scales= [1.3,1.4,1.3]
-    anonlinearity= 2.0
+    anonlinearity= 2.5
     
     # number of pixels the final image should have (in x and y, each)
     npix_final = 424
@@ -242,7 +307,6 @@ def mosaic_vs_cutout():
         thumbnail = f[0].data 
     with fits.open('%s/sv_v1.0_sc03_osect43_drz.fits' % mosaic_path) as f:
         mosaic = f[0].data
-    finput_name = 'randomselect_uniformwithmag_basicinfo.fits'
     with fits.open('%s/%s' % (basepath,finput_name)) as f:
         finput_data = f[1].data
     gal = finput_data[finput_data['UID_MOSAIC'] == 7416][0]
@@ -271,16 +335,54 @@ def mosaic_vs_cutout():
     fig.show()
 
     return None
-    
-if __name__ == '__main__':
 
-    """Test the code"""
-
-    ascales,anonlinearity,npix_final,apedestal = get_image_defaults()
-
-    finput_name = 'randomselect_uniformwithmag_basicinfo.fits'
+def get_mosaic_defaults(uid=7416):
 
     with fits.open('%s/%s' % (basepath,finput_name)) as f:
         finput_data = f[1].data
 
-    make_jpeg(finput_data[finput_data['UID_MOSAIC'] == 7416][0],ascales,anonlinearity,npix_final,apedestal,desaturate = False,show_img = True,load_from_mosaic = True)
+    gal=finput_data[finput_data['UID_MOSAIC'] == uid][0]
+
+    osect_z = gal['OSECT_Z']
+    with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'b',osect_z)) as f:
+        img_b = np.transpose(f[0].data)
+    with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'v',osect_z)) as f:
+        img_v = np.transpose(f[0].data)
+    with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'i',osect_z)) as f:
+        img_i = np.transpose(f[0].data)
+    with fits.open('%s/s%s_v1.0_sc03_osect%i_drz.fits' % (mosaic_path,'z',osect_z)) as f:
+        img_z = np.transpose(f[0].data)
+
+    mosaics = (img_b,img_v,img_i,img_z)
+
+    return mosaics
+
+def make_webpage():
+
+    with fits.open('%s/%s' % (basepath,finput_name)) as f:
+        finput_data = f[1].data
+
+    with open('/Users/willettk/Astronomy/meetings/uk2014/gzh_jpg/saturation_test.html','w') as f:
+        for gal in finput_data:
+            # Remove obvious stars
+            if np.max([gal['CLASS_STAR_B'],gal['CLASS_STAR_V'],gal['CLASS_STAR_I'],gal['CLASS_STAR_Z']]) < 0.75 and np.mean([gal['CLASS_STAR_B'],gal['CLASS_STAR_V'],gal['CLASS_STAR_I'],gal['CLASS_STAR_Z']]) <= 0.75 and gal['MAG_BEST_Z'] <= 24.5:
+                # Regular
+                f.write('<IMG SRC="saturation_test/no_desaturation/goods_s_g%s_bviz_thumb.jpg" TITLE="GOODS-S id:%s r_e_V[pix]=%.1f mag_V=%.2f r_e_z[pix]=%.1f mag_z=%.2f">' % \
+                    (gal['UID_MOSAIC'],gal['UID_MOSAIC'],gal['FLUX_RADIUS1_V'],gal['MAG_BEST_V'],gal['FLUX_RADIUS1_Z'],gal['MAG_BEST_Z']))
+                # Desaturated
+                f.write('<IMG SRC="saturation_test/desaturation/goods_s_g%s_bviz_thumb.jpg" TITLE="GOODS-S id:%s r_e_V[pix]=%.1f mag_V=%.2f r_e_z[pix]=%.1f mag_z=%.2f">' % \
+                    (gal['UID_MOSAIC'],gal['UID_MOSAIC'],gal['FLUX_RADIUS1_V'],gal['MAG_BEST_V'],gal['FLUX_RADIUS1_Z'],gal['MAG_BEST_Z']))
+                # Desaturated w/colored noise added back in 
+                f.write('<IMG SRC="saturation_test/desaturation_plus_color/goods_s_g%s_bviz_thumb.jpg" TITLE="GOODS-S id:%s r_e_V[pix]=%.1f mag_V=%.2f r_e_z[pix]=%.1f mag_z=%.2f"><br>\n' % \
+                    (gal['UID_MOSAIC'],gal['UID_MOSAIC'],gal['FLUX_RADIUS1_V'],gal['MAG_BEST_V'],gal['FLUX_RADIUS1_Z'],gal['MAG_BEST_Z']))
+
+    return None
+
+if __name__ == '__main__':
+
+    """Test the code"""
+
+    with fits.open('%s/%s' % (basepath,finput_name)) as f:
+        finput_data = f[1].data
+
+    make_jpeg(finput_data[finput_data['UID_MOSAIC'] == 7416][0],desaturate = False,show_img = False,load_from_mosaic = True)
